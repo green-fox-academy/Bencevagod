858a0c3d0fc909e8b1cf364a9a475b59224dcd98
Bencevagod <bajkaibence@gmail.com>
2020-05-25 19:04:36 +0200

    unique finally works, but it is ugly with the nulls in the list

--- week-02/day-1/Functions/src/Unique.java
+++ week-02/day-1/Functions/src/Unique.java
@@ -1,26 +1,28 @@
+import java.util.ArrayList;
+
 public class Unique {
     public static void main(String[] args) {
         int[] nums = {1, 11, 34, 11, 52, 61, 1, 34};
-        unique(nums);
-        for (int i = 0; i < nums.length; i++){
-            System.out.println(nums[i]);
+        int[] uniqueNums = unique(nums);
+        for (int i = 0; i <uniqueNums.length; i++){
+
+            System.out.println(uniqueNums[i]);
         }
     }
     public static int[] unique (int[] a){
         int[] uniqueList = new int[a.length];
         for (int i = 0; i < a.length; i++){
-            if (!isTrue(uniqueList, i)){
+            if (!isTrue(uniqueList, a[i])){
                 uniqueList[i] = a[i];
             }
         }
         return uniqueList;
     }
-    public static boolean isTrue (int[] b, int c){
-        for (int i = 0; i < b.length; i++){
-            if (b[i] != c){
+    public static boolean isTrue (int[] arr, int value){
+        boolean jelen = true;
+        for (int item : arr) {
+            if (item == value) {
                 return true;
-            } else {
-                return false;
             }
         }
         return false;
